fdd0c5fa40a7cf5a3c19335f752b0ed94bda560f
Polina Severina <polina.sever53@gmail.com>
2022-10-11 22:30:42 +0200

    task 11

--- hello.php
+++ hello.php
@@ -1,15 +1,9 @@
 <?php
 
-$value = 13;
-echo $value . "\n";
+$num_languages=4;
+$month=11;
 
-$secondValue = 13.67;
-echo $secondValue . "\n";
+$days=16*$month;
+$days_per_languages=$days/$num_languages;
 
-$a = 11;
-$b = 1;
-echo $a + $b . "\n";
-
-$last_month = 1187.23;
-$this_month = 1089.98;
-echo 'difference = ' . ($last_month - $this_month);
+echo $days_per_languages . "\n";
