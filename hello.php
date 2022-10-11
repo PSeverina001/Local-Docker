79ac4b6435031095781d846c0839e3cb360ff001
Polina Severina <polina.sever53@gmail.com>
2022-10-11 23:25:20 +0200

    task 14. Work with min/max

--- hello.php
+++ hello.php
@@ -1,10 +1,5 @@
 <?php
 
-$radical=sqrt(379);
-echo "До целых: " . round($radical,0) . "\n";
-echo "До десятых: " . round($radical,1) . "\n";
-echo "До сотых: " . round($radical,2) . "\n";
-
-$value = sqrt(587);
-$array=["floor" => floor($value), "ceil" => ceil($value)];
-print_r($array);
+$array = [4, -2, 5, 19, -130, 0, 10];
+echo "min = " . min($array) . "\n";
+echo "max = " . max($array) . "\n";
