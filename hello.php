5c96ccb7270d64fd9e008311005afe64479021c3
Polina Severina <polina.sever53@gmail.com>
2022-10-11 21:59:52 +0200

    task 1

--- hello.php
+++ hello.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "Hello, world!";
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+$order =& $very_bad_unclear_name;
+$order .= " and Coco-Cola!";
+echo "\nYour order is: $very_bad_unclear_name.";
