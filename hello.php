fea999a9aa086d35260e4ffdade50bd799cb885b
Polina Severina <polina.sever53@gmail.com>
2022-10-13 23:21:24 +0200

    task 15

--- hello.php
+++ hello.php
@@ -1,28 +1,11 @@
 <?php
 
-//задача1
-$dividers = [1];
-$num = 27;
-for ($i = 2; $i <= $num; $i++)
+function printStringReturnNumber(): int
 {
-	if ($num % $i === 0)
-	{
-		$dividers[] = $i;
-	}
-}
-print_r($dividers);
+	echo "Your number is ";
 
-//задача2
-$count = 0;
-$sum = 0;
-$array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-foreach ($array as $value)
-{
-	$sum += $value;
-	$count++;
-	if ($sum > 10)
-	{
-		echo $count . "\n";
-		break;
-	}
+	return rand(1, 100);
 }
+
+$my_num = printStringReturnNumber();
+echo $my_num;
