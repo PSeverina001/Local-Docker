2224a5fb49f7662ed7498d168dc397c1ef2783b7
Polina Severina <polina.sever53@gmail.com>
2022-10-14 20:38:55 +0200

    task 17.3

--- hello.php
+++ hello.php
@@ -1,13 +1,12 @@
 <?php
 
-function arrayFill($element, $count) : array
+$arr = [[1, 2, 3], [4, 5], [6]];
+$sum=0;
+for ($i = 0; $i < count($arr); $i++)
 {
-	$arr=[];
-	for ($i = 0; $i < $count; $i++)
+	for ($j = 0; $j < count($arr[$i]); $j++)
 	{
-		$arr[$i].=$element;
+		$sum+=$arr[$i][$j];
 	}
-	return $arr;
 }
-$arr=arrayFill('x',5);
-print_r($arr);
+echo $sum;
