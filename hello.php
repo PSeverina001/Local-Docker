15ad7090fd39f35572b15de41e3a620e02830d96
Polina Severina <polina.sever53@gmail.com>
2022-10-14 23:05:30 +0200

    task 20.5

--- hello.php
+++ hello.php
@@ -1,6 +1,10 @@
 <?php
 
-$keys = range('a', 'z');
-$values = range(1, 26);
-$arr = array_combine($keys, $values);
-print_r($arr);
+$str = '1234567890';
+$arr = str_split($str, 2);
+$sum = 0;
+for ($i = 0; $i < count($arr); $i++)
+{
+	$sum += $arr[$i];
+}
+echo $sum . "\n";
