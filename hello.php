fa72ab7d90704ff85c90090bba174fae3b2c7845
Polina Severina <polina.sever53@gmail.com>
2022-10-14 22:00:20 +0200

    task 19

--- hello.php
+++ hello.php
@@ -1,61 +1,10 @@
 <?php
 
-function sumMoreTen($a, $b): bool
+for($i=0;$i<20;$i++)
 {
-	if ($a + $b > 10)
+	for($j=0;$j<$i+1;$j++)
 	{
-		return true;
+		echo 'x';
 	}
-	else
-	{
-		return false;
-	}
-}
-
-function equalNumber($a, $b): bool
-{
-	if ($a === $b)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-echo $test = 0 ? "верно" : "ложь" . "\n";
-
-$age = 76;
-if ($age >= 10 and $age <= 99)
-{
-	$sum = 0;
-	while ($age > 0)
-	{
-		$sum += $age % 10;
-		$age /= 10;
-	}
-	if ($sum <= 9)
-	{
-		echo "сумма цифр однозначна!" . "\n";
-	}
-	else
-	{
-		echo "сумма цифр двузначна!" . "\n";
-	}
-}
-else
-{
-	echo "число меньше 10 и больше 99" . "\n";
-}
-
-$arr = [1, 4, 7];
-if (count($arr) === 3)
-{
-	$sum = 0;
-	for ($i = 0; $i < count($arr); $i++)
-	{
-		$sum += $arr[$i];
-	}
-	echo "сумма элементов массива = " . $sum;
+	echo "\n";
 }
