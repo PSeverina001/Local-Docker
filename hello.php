8d48afd02bc87c769c51e3f47fd4e2224c8f0a42
Polina Severina <polina.sever53@gmail.com>
2022-11-01 11:37:22 +0200

    task 1.a

--- hello.php
+++ hello.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Hello, world!";
+$str='ahb acb adca aeb aeeb abba adcb axeb';
+$regexp='/a(dc|b[eb])a/';
+$matches = array();
+
+preg_match_all($regexp, $str, $matches);
+var_dump($matches);
