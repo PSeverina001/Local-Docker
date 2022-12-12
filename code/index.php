1e12578eac3de0d670c0c44e3161c0d6957b6d0e
Polina Severina <polina.sever53@gmail.com>
2022-12-13 00:54:33 +0200

    Чистый код

--- code/index.php
+++ code/index.php
@@ -1,22 +1,9 @@
 <?php
 
-require_once ('../vendor/autoload.php');
-require_once ('../.env/constants.php');
+require_once "./function/render/renderFunction.php";
+require_once '../vendor/autoload.php';
 
-$mysqli=new mysqli('db','root','helloworld','web');
-if ($result=$mysqli->query('SELECT * FROM ad ORDER BY created DESC')){
-	$advertisement=$result->fetch_all();
-}
-
-//SET
-if (isset($_POST['email']) && isset($_POST['title']) && $_POST['category'] && $_POST['description'])
-
-{
-	$mysqli->query('INSERT INTO ad(email,title,category,description) 
-VALUES("'. $_POST['email'] .'","'.$_POST['title'] .'","'.$_POST['category'].'","'.$_POST['description'].'")');
-	Header('Location: '.$_SERVER['PHP_SELF']);
-}
-require_once('hello.php');
-?>
-
-
+$template = renderTemplate(
+	"./view/index.html"
+);
+renderLayout($template);
